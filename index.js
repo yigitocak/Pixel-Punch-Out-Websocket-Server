57d1dca7f35dfec7aebe7082882c2f10b46c8a21
yigit ocak <yigitockk@gmail.com>
2024-05-13 16:37:30 -0400

    Refactor server files and enhance game functionality
    Removed unnecessary files from the WebSocket server directory and relocated the essential ones to the root directory. Enhanced the game lifecycle by adding more robust logic to handle disconnects, start the game countdown, and manage player deaths. This refactor aims to keep the directory layout clean and improve the overall game functionality.

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import tick from "./tick.js";
+import SocketHandler from "./socketHandler.js"
+
+const app = express();
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    pingInterval: 2000,
+    pingTimeout: 5000,
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+const port = 5050;
+
+// Globals
+const players = [];
+const inputsMap = {};
+
+// Gravity
+const GRAVITY = 2;
+const JUMPSPEED = -30;
+
+// Socket Listeners
+const socketHandler = new SocketHandler(io, players, inputsMap);
+socketHandler.handleConnection();
+
+// Tick Loop
+setInterval(() => {
+    if (socketHandler.started) {
+        tick(players, inputsMap, GRAVITY, JUMPSPEED);
+        io.emit("playerInputs", inputsMap);
+        io.emit("players", players);
+    }
+}, 1000 / 30);
+
+server.listen(port, () => {
+    console.log(`Running on ${port}`);
+});
